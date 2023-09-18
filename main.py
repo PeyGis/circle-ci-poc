9a921fb5dd83d76a0b20a0ca2dd23eecbc55e997
pages_coffy <isaac.p.coffie@gmail.com>
2023-09-18 11:56:47 +0000

    add sample project

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+
+def say_hello():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    say_hello()
---
+++ test_main.py
@@ -0,0 +1,11 @@
+from main import say_hello, add, sub
+
+
+class TestCases():
+    def test_add():
+        assert add(2, 3) == 5
+        print("test add passed")
+    
+    def test_sub():
+        assert sub(5, 2) == 3
+        print("test sub passed")
